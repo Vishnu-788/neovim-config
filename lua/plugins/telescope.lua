66d2f5c1527bc59e25e2b9359bf93ed6dfbf0451
Vishnu Prasad J S <prasadvishnu788@gmail.com>
2026-06-29 16:32:00 +0530

    Fix: pressing q in cmd mode will exit telescope window.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,23 +1,38 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  version = "*",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    -- optional but recommended
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-  },
-  opts = {
-    defaults = {
-      -- This forces the vertical stack (results on top, preview on bottom) layout_strategy = "vertical",
-      layout_config = {
-        vertical = {
-          -- Adjust these to your preference
-          width = 0.9,
-          height = 0.9,
-          preview_height = 0.5, -- Preview window takes up 50% of the height
-          mirror = true, -- Setting mirror to true often places results on top
-        },
-      },
-    },
-  },
+   "nvim-telescope/telescope.nvim",
+   version = "*",
+   dependencies = {
+      "nvim-lua/plenary.nvim",
+      -- optional but recommended
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+   },
+   -- Single opts function that returns ALL your settings
+   opts = function()
+      local actions = require("telescope.actions")
+
+      return {
+         defaults = {
+            -- FIX: Moved to a new line so it isn't commented out
+            layout_config = {
+               vertical = {
+                  -- Adjust these to your preference
+                  width = 0.9,
+                  height = 0.9,
+                  preview_height = 0.5, -- Preview window takes up 50% of the height
+                  mirror = true,     -- Setting mirror to true often places results on top
+               },
+            },
+            -- Mapping settings
+            mappings = {
+               i = {
+                  ["<esc>"] = false,
+               },
+               n = {
+                  ["<esc>"] = false,
+                  ["q"] = actions.close,
+               },
+            },
+         },
+      }
+   end,
 }
